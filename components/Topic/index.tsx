378aed1c51554248bc08d9269899dec56ade39f9
suganya05 <go2suganya@gmail.com>
2022-11-24 22:58:42 +0530

    chore: home page responsive

--- components/Topic/index.tsx
+++ components/Topic/index.tsx
@@ -2,49 +2,65 @@ import React from "react";
 import styles from "../Topic/Topic.module.scss";
 import { DemoTopic } from "./Topic";
 import Image from "next/image";
+import Slider from "react-slick";
 
 const Topic = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1402,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  };
   return (
-    // <div className={styles.topicContainer}>
-    //   <div className="mx pad">
-    //     <h3>Topic</h3>
-    //     <div className={styles.topicWrapper}>
-    //       {DemoTopic.map((f, index) => {
-    //         return (
-    //           <div key={index} className={styles.topicContent}>
-    //             <div className={styles.topicImg}>
-    //               <Image
-    //                 src={f.image.src}
-    //                 alt={f.image.alt}
-    //                 width={263}
-    //                 height={160}
-    //               />
-    //             </div>
-    //             <div className={styles.topicTitle}>
-    //               <h5>{f.title}</h5>
-    //             </div>
-    //           </div>
-    //         );
-    //       })}
-    //     </div>
-    //   </div>
-    // </div>
     <div className={styles.topicContainer}>
       <div className="mx pad">
         <h3>Topic</h3>
         <div className={styles.topicWrapper}>
-          {DemoTopic.map((f, index) => {
-            return (
-              <div key={index} className={styles.topicContent}>
-                <div className={styles.topicImg}>
-                  <Image src={f.image.src} alt={f.image.alt} layout="fill" />
+          <Slider {...settings}>
+            {DemoTopic.map((f, index) => {
+              return (
+                <div key={index} className={styles.topicContent}>
+                  <div className={styles.topicImg}>
+                    <Image src={f.image.src} alt={f.image.alt} layout="fill" />
+                  </div>
+                  <div className={styles.topicTitle}>
+                    <h5>{f.title}</h5>
+                  </div>
                 </div>
-                <div className={styles.topicTitle}>
-                  <h5>{f.title}</h5>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </Slider>
         </div>
       </div>
     </div>
--- home/News/index.tsx
+++ home/News/index.tsx
@@ -12,6 +12,35 @@ const News = () => {
     slidesToShow: 4,
     slidesToScroll: 2,
     autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          infinite: true,
+          // dots: true,
+        },
+      },
+      {
+        breakpoint: 873,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          // dots: true,
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          // dots: true,
+        },
+      },
+    ],
   };
   return (
     <div className={styles.newsContainer}>
